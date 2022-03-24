2862ea625dcb89806410a2b5a6dbb37715910839
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2022-03-24 20:54:15 +0600

    if vowel

--- concatenate.js
+++ concatenate.js
@@ -1,3 +1,4 @@
+/*
 var firstname= "anisul islam"
 var lastname = " rubel"
 var fullname= firstname + lastname;
@@ -6,3 +7,4 @@ var num = 30;
 var num2 = 40;
 var sum= num+num2;
 console.log("num = "+num+"+"+num2+"="+sum)
+*/
