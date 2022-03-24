ffbb3558b8598fda0c8752750891fd60bc166401
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2022-03-24 12:17:15 +0600

    num,fixed,toPrecision,toString,toInt

---
+++ index.js
@@ -0,0 +1,15 @@
+// alert("Dolon");
+console.log(" I love bangladesh");
+document.write("Al AMin sarker Dolon <br/>");
+document.write("Daffodil International University");
+
+var num = 2.567;
+var name = "dolon"
+console.log(num);
+console.log(num.toFixed(3)) // . ar por number
+console.log(typeof(toString(num)))
+console.log(typeof(parseInt(num)))
+console.log(num.toPrecision(3));//total number
+console.log(Number(4))
+console.log(typeof(Number(45)))
+
