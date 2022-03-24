3fe6edfe91885051418937e21d26e084e903ba86
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2022-03-24 16:58:57 +0600

    before if/else

---
+++ concatenate.js
@@ -0,0 +1,8 @@
+var firstname= "anisul islam"
+var lastname = " rubel"
+var fullname= firstname + lastname;
+console.log(fullname)
+var num = 30;
+var num2 = 40;
+var sum= num+num2;
+console.log("num = "+num+"+"+num2+"="+sum)
